4fb13e4d7947f45945fef936121764347fee9ad4
Alberto Mateo Alcalá <amateo1982@yahoo.es>
2020-07-06 13:53:59 +0200

    First commit. Starting the public website

---
+++ config.rb
@@ -0,0 +1,10 @@
+require 'compass/import-once/activate'
+
+http_path = "/"
+css_dir = "css"
+sass_dir = "sass"
+images_dir = "images"
+javascripts_dir = "js"
+
+output_style = :compressed
+line_comments = false
